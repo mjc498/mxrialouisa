14d45dc4aa8f9f939ac47ac54c3e04d4106fc059
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-02-18 23:22:03 -0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<main class="content-wrapper">
+    <section class="error-404">
+        <h1>404 Page Not Found</h1>
+        <p>Sorry, we can’t find the page you’re looking for :(</p>
+
+        <!-- BUTTON -->
+        <a href="<?php echo home_url(); ?>" class="back-home-btn">Back to Home</a>
+    </section>
+</main>
+<?php get_footer(); ?>
