a24d78d92d1e17d19beea0547856d355606bcbaa
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-02-18 23:48:10 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
-<main class="content-wrapper">
-    <section class="error-404">
-        <h1>404 Page Not Found</h1>
-        <p>Sorry, we can’t find the page you’re looking for :(</p>
-
-        <!-- BUTTON -->
-        <a href="<?php echo home_url(); ?>" class="back-home-btn">Back to Home</a>
-    </section>
+<main>
+    <h1 class="error-page">
+        404<br>
+        Page Not Found
+    </h1>
+    <p>Sorry, we can't find the page you're looking for :(</p>
+    <button class="custom-button">
+        <a href="<?php echo home_url(); ?>">Back to Home</a>
+    </button>
 </main>
 <?php get_footer(); ?>
